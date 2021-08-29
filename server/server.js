8231d037e63f81be7eb2f4d427247e0a2f326450
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-29 16:47:11 -0400

    basic login auth stuff

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "a7b7c0829bf44999bfc49960237dced2",
+    clientSecret: "8781b6ef0b3e42c588828f2e371f373f",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
