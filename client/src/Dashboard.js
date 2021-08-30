c7184f400a0d065be4efb329b450c226f266da87
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-30 15:17:55 -0400

    Fully working, may need cleaning

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -3,6 +3,7 @@ import useAuth from "./useAuth";
 import { Container, Form } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-node";
 import TrackSearchResults from "./TrackSearchResults";
+import Player from "./Player";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: "a7b7c0829bf44999bfc49960237dced2",
@@ -12,6 +13,12 @@ export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    setSearch("");
+  }
 
   useEffect(() => {
     if (!accessToken) return;
@@ -57,10 +64,16 @@ export default function Dashboard({ code }) {
       />
       <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
         {searchResults.map((track) => (
-          <TrackSearchResults track={track} key={track.uri} />
+          <TrackSearchResults
+            track={track}
+            key={track.uri}
+            chooseTrack={chooseTrack}
+          />
         ))}
       </div>
-      <div>Bottom</div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
     </Container>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
 const cors = require("cors");
@@ -10,9 +11,9 @@ app.use(bodyParser.json());
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "a7b7c0829bf44999bfc49960237dced2",
-    clientSecret: "",
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     refreshToken,
   });
   spotifyApi
@@ -31,9 +32,9 @@ app.post("/refresh", (req, res) => {
 app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "a7b7c0829bf44999bfc49960237dced2",
-    clientSecret: "",
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
   });
 
   spotifyApi
