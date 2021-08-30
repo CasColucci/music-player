8f105417a17469a034764a868ca39836b092ba96
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-30 14:39:29 -0400

    Continuing to set up a basic player

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,7 +1,66 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
+import SpotifyWebApi from "spotify-web-api-node";
+import TrackSearchResults from "./TrackSearchResults";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "a7b7c0829bf44999bfc49960237dced2",
+});
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
-  return <div>{code}</div>;
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    let cancel = false;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
+  return (
+    <Container className="d-flex flex-column py-2 " style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search songs/artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        {searchResults.map((track) => (
+          <TrackSearchResults track={track} key={track.uri} />
+        ))}
+      </div>
+      <div>Bottom</div>
+    </Container>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -7,6 +7,27 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "a7b7c0829bf44999bfc49960237dced2",
+    clientSecret: "",
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
 app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
